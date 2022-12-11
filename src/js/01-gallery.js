c3a539261a2713d0a33de40581e0e861e5837cb8
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-11 18:32:46 +0100

    update browserlist + task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+'use strict';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(el => {
+  const galleryElement = `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`;
+  gallery.insertAdjacentHTML('beforeend', galleryElement);
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox', () => console.log('Opening modal window'));
+lightbox.on('close.simplelightbox', () => console.log('Closing modal window'));

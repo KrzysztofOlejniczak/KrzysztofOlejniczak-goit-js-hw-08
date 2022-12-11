e020874c4c1df2e96b9bdbf2a1918b9913e6a24a
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-11 22:34:21 +0100

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
 'use strict';
+
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new vimeoPlayer(iframe);
+
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  console.log(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+const saveVideoTime = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  //   console.log(data.seconds);
+};
+
+player.on('timeupdate', throttle(saveVideoTime, 1000));
